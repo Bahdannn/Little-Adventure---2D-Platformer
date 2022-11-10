e49f11b8c2761c4bd3d4a048b018e6f30e7d8743
Bahdannn <diachuk.b@gmail.com>
2022-11-11 02:43:06 +0300

    Движение, прыжки, камера

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int lives = 5;
+    [SerializeField] private float jumpForse = 5f;
+    
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+    
+
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
+        
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForse, ForceMode2D.Impulse);
+    }
+}
