a66476384c31ee22dd758e4898d2969f2a4d3c48
Bahdannn <diachuk.b@gmail.com>
2022-11-30 15:17:41 +0300

    Добавлена Движущаяся платформа (Moving Platform + Rotation)

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Transform center;
+    [SerializeField] private float radius = 2f, angularSpeed = 2f;
+    private float positionX, positionY, angle = 0f;
+
+    public void Update()
+    {
+        positionX = center.position.x + Mathf.Cos(angle) * radius;
+        positionY = center.position.y + Mathf.Sin(angle) * radius;
+        transform.position = new Vector2(positionX, positionY);
+        angle = angle + Time.deltaTime * angularSpeed;
+
+        if (angle >= 360f)
+        {
+            angle = 0f;
+        }
+
+    }
+}
