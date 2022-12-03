0ff99180c730f82a1585f2c1d14420bdb0b125b7
Bahdannn <diachuk.b@gmail.com>
2022-12-03 03:54:31 +0300

    Добавлена пауза

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PausePanel;
+
+    public void PauseButtonPressed()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ContinueButtonPressed()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
